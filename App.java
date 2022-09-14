b7d6e3abad75f7df65abcd81c44d04f4c58ae005
lsavila <luca.avila@edu.ufes.br>
2022-09-14 09:28:07 -0300

    colocando os arquivos

---
+++ App.java
@@ -0,0 +1,10 @@
+import apoio.Time;
+
+public class App {
+    public static void main(String[] args) {
+        Time timeA = new Time();
+        timeA.setNome("Flamengo");
+        System.out.println(timeA.getNome());
+    
+    }
+}
---
+++ apoio/Time.java
@@ -0,0 +1,10 @@
+package apoio;
+public class Time {
+    private String nome;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
